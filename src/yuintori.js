e9d2a6d9215f358201755514740489ec7bec8372
rxon <rxon@users.noreply.github.com>
2017-08-25 20:08:14 +0900

    Move install to scraper.js

--- src/yuintori.js
+++ src/yuintori.js
@@ -3,13 +3,14 @@ const url = require('url');
 const path = require('path');
 
 const util = require('util');
-const execFile = util.promisify(require('child_process').execFile);
+// const execFile = util.promisify(require('child_process').execFile);
+const execFile =require('child_process').execFileSync;
 
 const axios = require('axios');
 const scraper = require('./scraper');
 
 scraper('yui_ogura_official').then(async posts => {
-  const postInfo = posts.map(post => inst(post));
+  const postInfo = posts.map(async post => await inst(post));
   await Promise.all(postInfo);
 });
 
@@ -25,58 +26,68 @@ function inst(post) {
       .then(function(response) {
         response.data.pipe(fs.createWriteStream(filename));
       })
-      .then(async () => await tri(post))
+      .then(function() {
+        // execFile('gm', [
+        //   'convert',
+        //   filename,
+        //   '-fuzz',
+        //   '5%',
+        //   '-trim',
+        //   '+repage',
+        //   'd' + filename
+        // ]);
+        execFile('convert', [
+          filename,
+          '-fuzz',
+          '5%',
+          '-trim',
+          '+repage',
+          'd' + filename
+        ]);
+      })
+      // .then(() => tri(post))
       .catch(err => reject(err.stack));
   });
 }
 
-  async tri(post) {
-  return new Promise(function(resolve, reject) {
-    const parsedUrl = url.parse(post.urls[0]);
-    const filename = path.basename(parsedUrl.pathname);
+async function tri(post) {
+  const parsedUrl = url.parse(post.urls[0]);
+  const filename = path.basename(parsedUrl.pathname);
 
+  // const { stdout } = await execFile(
+  //   'convert',
+  //   [filename, '-fuzz', '5%', '-trim', '+repage', 'd' + filename],
+  //   {encoding: 'binary', maxBuffer: 5000*1024}
+  // );
+  execFile('convert', [
+    filename,
+    '-fuzz',
+    '5%',
+    '-trim',
+    '+repage',
+    'd' + filename
+  ]);
 
-
- // convert -fuzz 5% -trim +repage yui.jpg
-      const { stdout } = await execFile('node', ['--version'],{ maxBuffer: 1024 * 1024 });
-      console.log(stdout.length);
-
-
-    getVersion();
-
-
-
-    // const readStream = fs.createReadStream(filename);
-    gm(filename)
-      // gm(filename)
-      .command('convert')
-      .fuzz('5%')
-      // .in('-fuzz', '5%')
-      // .in('-trim')
-      .trim()
-      // .write('d' + filename, function(err) {
-      //   if (!err) resolve('done');
-      //   // console.log('done');
-      // });
-      // .stream(function(err, stdout, stderr) {
-      //   var writeStream = fs.createWriteStream('d' + filename);
-      //   stdout.pipe(writeStream);
-      // })
-      .stream(function(err, stdout, stderr) {
-        // const writeStream = fs.createWriteStream('d' + filename);
-        let chunks = [];
-        stdout.on('data', function(chunk) {
-          console.log(chunk.length);
-          chunks.push(chunk);
-        });
-        stdout.on('end', function() {
-          const image = Buffer.concat(chunks);
-          console.log(image.length);
-          // writeStream.write(image.toString('base64'), 'base64');
-          fs.writeFileSync('d' + filename, image);
-
-          // writeStream.end();
-        });
-      });
-  });
+  await execFile('gm', [
+    'convert',
+    filename,
+    '-fuzz',
+    '5%',
+    '-trim',
+    '+repage',
+    'd' + filename
+  ]);
+  // let chunks = [];
+  // stdout.on('data', function(chunk) {
+  //   console.log(chunk.length);
+  //   chunks.push(chunk);
+  // });
+  // stdout.on('close', function() {
+  //   const image = Buffer.concat(chunks);
+  //   console.log(image.length);
+  //   // writeStream.write(image.toString('base64'), 'base64');
+  //   fs.writeFileSync('d' + filename, image);
+  //
+  //   // writeStream.end();
+  // });
 }
