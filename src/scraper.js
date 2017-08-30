7a4116bce4dbe9016d4f20816356e6349c24384d
rxon <rxon@users.noreply.github.com>
2017-08-30 17:55:13 +0900

    Add carousel task(kuso code)

--- src/scraper.js
+++ src/scraper.js
@@ -6,6 +6,23 @@ const filter = require('lodash.filter');
 
 const db = require('./db');
 
+function download(item, trim) {
+  const originalSizeUrl = item[item.type + 's'].standard_resolution.url;
+  const parsedUrl = url.parse(originalSizeUrl);
+  const filename = path.basename(parsedUrl.pathname);
+  axios({
+    method: 'get',
+    url: originalSizeUrl,
+    responseType: 'stream'
+  }).then(function(response) {
+    const writeStream = response.data.pipe(fs.createWriteStream(filename));
+    writeStream.on('finish', function() {
+      trim(filename);
+    });
+  });
+  return filename;
+}
+
 module.exports = async function(username, trim) {
   const endPoint = url.format({
     protocol: 'https',
@@ -20,27 +37,24 @@ module.exports = async function(username, trim) {
   });
 
   filteredMedia.forEach(item => {
-    const originalSizeUrl = item[item.type + 's'].standard_resolution.url;
-    const parsedUrl = url.parse(originalSizeUrl);
-    const filename = path.basename(parsedUrl.pathname);
-    if (item.created_time > lastYui) {
-      // db.set('last_yui', item.created_time).write();
+    if (item.type === 'carousel') {
+      // item.carousel_media.forEach(carouselItem => {
+      //   download(carouselItem, filename, trim);
+      // });
+      newMedia.push({
+        text: item.caption.text,
+        filename: download(item.carousel_media[0], trim)
+      });
+    } else {
+      newMedia.push({
+        text: item.caption.text,
+        filename: download(item, trim)
+      });
     }
 
-    axios({
-      method: 'get',
-      url: originalSizeUrl,
-      responseType: 'stream'
-    }).then(function(response) {
-      const writeStream = response.data.pipe(fs.createWriteStream(filename));
-      writeStream.on('finish', function() {
-        trim(filename);
-      });
-    });
-    newMedia.push({
-      text: item.caption.text,
-      filename
-    });
+    // if (item.created_time > lastYui) {
+    //   db.set('last_yui', item.created_time).write();
+    // }
   });
   return newMedia;
 };
